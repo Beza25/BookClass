fec995b6be787b612b14b258be7c29324ff5cec8
rramdat95 <ryan.t.ramdat@gmail.com>
2019-09-16 14:23:12 -0400

    second commit

--- src/Book.java
+++ src/Book.java
@@ -4,7 +4,50 @@ public class Book {
     private String author;
     private String description;
     private double price;
-    private Boolean isInStock = false;
+    private int isInStock;
+
+
+
+    public Book(){
+
+    }
+
+    public Book (  String author, String bookTitle, String description){
+        this.author = author;
+        this.bookTitle = bookTitle;
+        this.description = description;
+    }
+    public Book (  String author, String bookTitle, String description, double price, int isInStock){
+        this.author = author;
+        this.bookTitle = bookTitle;
+        this.description = description;
+        this.price = price;
+        this.isInStock = isInStock;
+
+    }
+    public double caluclatePrice(int userInput){
+        double bookPrice = 0;
+        if( this.isInStock >= userInput){
+            bookPrice = userInput * this.price;
+        }
+        return bookPrice;
+    }
+
+    public String getDisplayTitle(){
+
+        return "The author of " + getBookTitle() + " is " + getAuthor() + ", and it is a " + getDescription();
+
+
+    }
+
+    public int getIsInStock() {
+        return isInStock;
+    }
+
+    public void setIsInStock(int isInStock) {
+        this.isInStock = isInStock;
+    }
+
 
     public String getBookTitle() {
         return bookTitle;
@@ -36,29 +79,6 @@ public class Book {
 
     public void setPrice(double price) {
         this.price = price;
-    }
-
-    public Boolean getInStock() {
-        return isInStock;
-    }
-
-    public void setInStock(Boolean inStock) {
-        isInStock = inStock;
-    }
-
-    public Book(){}
-
-    public Book (  String author, String bookTitle, String description){
-        this.author = author;
-        this.bookTitle = bookTitle;
-        this.description = description;
-    }
-
-    public String getDisplayTitle(){
-
-        return "The author of " + getBookTitle() + " is " + getAuthor() + ", and it is a " + getDescription();
-
-
     }
 
 }
--- src/BookApp.java
+++ src/BookApp.java
@@ -1,7 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class BookApp {
     public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
 
-        Book b1 = new Book("Michelle Obama", "Becoming Michelle", "Memoir.");
+        Book b1 = new Book("Michelle Obama", "Becoming Michelle", "Memoir." );
         System.out.println( b1.getDisplayTitle());
+        Book book2 = new Book("Paula Hawkins", "The Girl on the train", "fiction", 22.5, 5 );
+        System.out.println( book2.getDisplayTitle());
+
+        System.out.println("Enter the title of the book you would like to purchase");
+        String book = input.nextLine();
+
+        System.out.println("How many do you want?");
+        int userInput = input.nextInt();
+
+        if(book.equalsIgnoreCase(book2.getBookTitle())){
+            System.out.println("Please pay $" + book2.caluclatePrice(userInput));
+
+        }else{
+            System.out.println("The book you requested is out of stock. Try another time!");
+        }
+        System.out.println("Bye!");
+
+
+
+
+
     }
+
+
 }
